08149a8a2f613fe44efcff4179821a5c05b16426
tamilamuthu26 <enterthemailid2023@gmail.com>
2024-09-30 19:13:10 +0530

    Employee management system crud operations

---
+++ crudApp/forms.py
@@ -0,0 +1,10 @@
+from django import forms  
+from crudApp.models import Employee  
+class EmployeeForm(forms.ModelForm):  
+    class Meta:  
+        model = Employee  
+        fields = ['name', 'contact', 'email'] 
+        widgets = { 'name': forms.TextInput(attrs={ 'class': 'form-control' }), 
+            'email': forms.EmailInput(attrs={ 'class': 'form-control' }),
+            'contact': forms.TextInput(attrs={ 'class': 'form-control' }),
+      }
